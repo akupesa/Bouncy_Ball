d68780ee3d0d01912f8d1d62ff75a99df8792ed1
Anderson Kupesa <akupesa@c1r10s7.42luanda.com>
2025-04-30 16:44:39 +0100

    Player Movement
    Player Movement Left
    Player Movement Right

--- Bouncy_Ball/sources/actors/Actor.gd
+++ Bouncy_Ball/sources/actors/Actor.gd
@@ -4,8 +4,6 @@ class_name Actor
 export var speed: = Vector2(300.0, 1000.0)
 export var gravity: = 3000.0
 
+var FLOOR_NORMAL = Vector2.UP
 var velocity: = Vector2.ZERO
 
-func	_physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity)
--- Bouncy_Ball/sources/actors/Player.gd
+++ Bouncy_Ball/sources/actors/Player.gd
@@ -1,1 +1,26 @@
 extends Actor
+
+func	_physics_process(delta: float) -> void:
+	var direction: = get_direction()
+	velocity = calcutale_move_velocity(velocity, direction, speed)
+	velocity = move_and_slide(velocity)
+	
+func	get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-1.0 
+		if Input.is_action_just_pressed("jump") and is_on_ceiling()
+		else 1.0
+	)
+	
+func	calcutale_move_velocity(
+		linear_velocity: Vector2,
+		direction: Vector2,
+		speed: Vector2
+	) -> Vector2:
+		var new_velocity: = linear_velocity
+		new_velocity.x = speed.x * direction.x
+		velocity.y += gravity * get_physics_process_delta_time()
+		if direction.y == -1.0:
+			velocity.y = speed.y * direction.y
+		return speed * direction
